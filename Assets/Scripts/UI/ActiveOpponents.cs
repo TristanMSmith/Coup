9da6776f3e09e835010a106966c3fd219b00af4a
Tristan Smith <30475619+TristanMSmith@users.noreply.github.com>
2021-01-28 18:36:35 -0500

    Updated Influence Animation

--- Assets/Scripts/UI/ActiveOpponents.cs
+++ Assets/Scripts/UI/ActiveOpponents.cs
@@ -8,18 +8,6 @@ public class ActiveOpponents : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //foreach scriptable object in the assets folder, instantiate an interaction prefab
-        //and reassign the interactionPrefab's properties to the s.o.'s
-        //TODO: Allow the host to decide which interactions are available
-
-        Interaction[] interactions = Resources.LoadAll<Interaction>("ScriptableObjects");
-        foreach (Interaction interaction in interactions)
-        {
-            GameObject interactionGameObject = Instantiate(opponentPrefab, transform);
-            B_Interaction b_interaction = interactionGameObject.GetComponent<B_Interaction>();
-            b_interaction.interaction = interaction;
-            interactionGameObject.name = interaction.name;
-        }
     }
 
     void Add()
